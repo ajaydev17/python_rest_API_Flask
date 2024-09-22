e19feed30f6d546c763ca4aeb2929b164e3e8c89
Ajay Dev <devadigaajay1729@gmail.com>
2024-09-22 10:48:33 -0400

    Added docker file for creating a flask server

---
+++ Project-1 Restful API/web/app.py
@@ -0,0 +1,116 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+def validate_data(data, function_name):
+    if function_name in ['addition', 'substraction', 'multiplication']:
+        if 'a' not in data and 'b' not in data:
+            return 301
+        else:
+            return 301
+    elif function_name == 'division':
+        if 'a' not in data and 'b' not in data:
+            return 301
+        elif int(data.get('b')) == 0:
+            return 302
+        else:
+            return 200
+
+
+class Add(Resource):
+    def post(self):
+        # parse the request JSON body
+        data = request.get_json()
+
+        status_code = validate_data(data, 'addition')
+
+        if status_code != 200:
+            return {
+                'message': 'Invalid number of data',
+                'status_code': status_code
+            }
+
+        sum = data.get('a', 0) + data.get('b', 0)
+        result = {
+            'value': sum,
+            'status_code': status_code
+        }
+
+        return jsonify(result)
+
+
+class Substract(Resource):
+    def post(self):
+        # parse the request JSON body
+        data = request.get_json()
+
+        status_code = validate_data(data, 'substraction')
+
+        if status_code != 200:
+            return {
+                'message': 'Invalid number of data',
+                'status_code': status_code
+            }
+
+        sum = data.get('a', 0) - data.get('b', 0)
+        result = {
+            'value': sum,
+            'status_code': status_code
+        }
+
+        return jsonify(result)
+
+
+class Multiply(Resource):
+    def post(self):
+        # parse the request JSON body
+        data = request.get_json()
+
+        status_code = validate_data(data, 'multiplication')
+
+        if status_code != 200:
+            return {
+                'message': 'Invalid number of data',
+                'status_code': status_code
+            }
+
+        product = data.get('a', 0) * data.get('b', 0)
+        result = {
+            'value': product,
+            'status_code': status_code
+        }
+
+        return jsonify(result)
+
+
+class Divide(Resource):
+    def post(self):
+        # parse the request JSON body
+        data = request.get_json()
+
+        status_code = validate_data(data, 'division')
+
+        if status_code != 200:
+            return {
+                'message': 'An error occurred',
+                'status_code': status_code
+            }
+
+        value = int(data.get('a', 0)) * 1.0 / int(data.get('b', 0))
+        result = {
+            'value': value,
+            'status_code': status_code
+        }
+
+        return jsonify(result)
+
+# Add resource to the API
+api.add_resource(Add, '/addition')
+api.add_resource(Substract, '/substraction')
+api.add_resource(Multiply, '/multiplication')
+api.add_resource(Divide, '/division')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
